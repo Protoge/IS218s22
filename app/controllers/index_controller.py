5942a3c7fe9a28eab1ca50d09e77ee25e8d312b7
rj <rjgilo2799@gmail.com>
2022-02-03 20:41:34 -0500

    change name and added screenshot

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Keith Williams"
+        name = "Rene Gilo"
         return render_template('index.html', name=name)
